120384bbdc9d0ee5b6f4b96949871045abe367f1
nivid-koradiya <cosmicexpense.it@gmail.com>
2023-09-13 19:14:34 +0530

    sidebar for admin edited

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
+      <b>T&C* Apply</b>
     </div>
-    <strong>Copyright &copy; 2018 Brought To You By<a href="https://code-projects.org/">Code-Projects</a></strong>
+    <strong>Copyright &copy; 2023 <a href="https://code-projects.org/">Fasal Sahayak</a></strong>
 </footer>
